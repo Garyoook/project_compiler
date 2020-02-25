d1dfc4810fd443a1b957656e4de0eed07ba6c485
Wenteng Zhao <wz5918@corona62.doc.ic.ac.uk>
2020-02-25 14:35:33 +0000

    [cl10418] put functionparams immediatly after visitparams

--- src/main/doc/wacc/utils/ASTVisitor.java
+++ src/main/doc/wacc/utils/ASTVisitor.java
@@ -304,6 +304,8 @@ public class ASTVisitor {
     for (ParamNode p: ast.getParameters()) {
       visitParamNode(p);
     }
+    functionParams.put(ast.getFuncName(), symbolTable.getParamCounter());
+
     codes.add("f_" + ast.getFuncName() + ":");
     codes.add(PUSH(LR));
     visitStat(ast.getFunctionBody(), codes, reg_counter);
@@ -313,7 +315,6 @@ public class ASTVisitor {
     codes.add(POP(PC));
     codes.add(POP(PC));
     codes.add("\t.ltorg");
-    functionParams.put(ast.getFuncName(), symbolTable.getParamCounter());
     symbolTable = symbolTabletemp;
     // TODO: 18/02/2020 saveReg restoreReg
 //    restoreReg();
